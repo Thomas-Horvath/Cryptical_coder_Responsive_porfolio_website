572d5396908573b6b7810b94bea8c67251c517b1
Thomas-Horvath <thomas.horvath0416@gmail.com>
2023-11-01 21:14:29 +0100

    Start project, basic setup, variables

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*=============== SHOW SIDEBAR ===============*/
+
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+
+
+/*=============== SKILLS TABS ===============*/
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/*===== Link Active Work =====*/
+
+
+/*===== Work Popup =====*/
+
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== INPUT ANIMATION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
