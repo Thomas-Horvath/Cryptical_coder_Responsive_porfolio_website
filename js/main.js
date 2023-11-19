f61404285a3f988adb5e96ed7fb8ee7495da4242
Thomas-Horvath <thomas.horvath0416@gmail.com>
2023-11-19 20:15:23 +0100

    skills section finish

--- js/main.js
+++ js/main.js
@@ -10,8 +10,24 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target"),
+    tabcontent = document.querySelectorAll('[data-content');
 
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
 
+        tabcontent.forEach(tabcontents => {
+         tabcontents.classList.remove("skills__active")   
+        })
+        target.classList.add("skills__active")
+
+        tabs.forEach(tab => {
+         tab.classList.remove("skills__active")   
+        })
+        tab.classList.add("skills__active")
+    })
+})
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
