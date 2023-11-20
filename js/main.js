5901a734254c076fa666025006c1153668177b62
Thomas-Horvath <thomas.horvath0416@gmail.com>
2023-11-20 19:11:57 +0100

    portfolio popup finish

--- js/main.js
+++ js/main.js
@@ -48,8 +48,23 @@ function activeWork() {
 linkWork.forEach(x => x.addEventListener("click", activeWork))
 
 /*===== Work Popup =====*/
+document.addEventListener("click" , (e) => {    
+    if(e.target.classList.contains("work__button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    } 
+})
 
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+document.querySelector('.portfolio__popup-close').addEventListener("click", togglePortfolioPopup);
 
+function portfolioItemDetails(portfolioItem) {
+   document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+   document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+   document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
 /*=============== SERVICES MODAL ===============*/
 
 
