f1ab1a8f50f12525a3ad03f152b0c5c83ed2eaa3
Thomas-Horvath <thomas.horvath0416@gmail.com>
2023-11-21 18:21:33 +0100

    services section

--- js/main.js
+++ js/main.js
@@ -65,7 +65,44 @@ function portfolioItemDetails(portfolioItem) {
    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
 }
+
+
+
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll(".services__modal"),
+      modalBtns = document.querySelectorAll(".services__button"),
+      modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add("active-modal");
+}     
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener("click", () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove("active-modal");
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
