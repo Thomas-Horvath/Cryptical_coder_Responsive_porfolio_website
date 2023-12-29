4ebd371f10c057af7958e79597c2d2d1387a49d4
Thomas-Horvath <thomas.horvath0416@gmail.com>
2023-12-29 19:46:21 +0100

    active link scroll function

--- js/main.js
+++ js/main.js
@@ -166,7 +166,38 @@ inputs.forEach((input) => {
     input.addEventListener("focus", focusFunc);
     input.addEventListener("blur", blurFunc);
 })
+
+
+
+
+
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', navHighlighter);
+
+function navHighlighter() {
+    let scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 30,
+        sectionId = current.getAttribute('id');
+
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+
+    })
+
+}
+
 
 
 /*=============== SHOW SCROLL UP ===============*/
